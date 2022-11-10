2b090b22101a1901e1756284c443ac92c3d9fa9a
chebishev <atanas.chebishev@gmail.com>
2022-11-10 12:23:09 +0200

    It already passes through the cookie consent

---
+++ sourceforge_new_folder_checker.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import time
+
+# set location of our edge driver
+edge_driver_path = "edgedriver_win64/msedgedriver.exe"
+driver = webdriver.Edge(edge_driver_path)
+
+# set the url that we will check for new folders
+target_url = "https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/files/xiaomi.eu/MIUI-WEEKLY-RELEASES/"
+
+# opens the web page
+driver.get(target_url)
+
+# wait x seconds before searching for the consent button
+time.sleep(3)
+# locate and click the "do not accept" button
+consent_button = driver.find_element_by_xpath('//*[@id="cmpwelcomebtnno"]/a')
+consent_button.click()
