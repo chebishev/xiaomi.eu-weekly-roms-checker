b114eecaa399339e8d71ff37654bb6fa432c86e8
chebishev <atanas.chebishev@gmail.com>
2023-02-04 16:20:11 +0200

    added headless mode

--- sourceforge_new_folder_checker.py
+++ sourceforge_new_folder_checker.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
+from msedge.selenium_tools import EdgeOptions
 from datetime import datetime
-import time
 
-# set location of our edge driver (you can get it from here:
-# https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
-edge_driver_path = "msedgedriver.exe"
-driver = webdriver.Edge(edge_driver_path)
+# options - in order to make headless search with Edge
+options = EdgeOptions()
+options.use_chromium = True
+options.add_argument("--headless")
+options.add_argument("disable-gpu")
+options.add_argument('--allow-running-insecure-content')
+options.add_argument('--ignore-certificate-errors')
+
+driver = webdriver.Chrome(executable_path='msedgedriver.exe', options=options)
 
 # set the url that we will check for new folders
 target_url = "https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/files/xiaomi.eu/MIUI-WEEKLY-RELEASES/"
@@ -13,34 +18,46 @@ target_url = "https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/fil
 # opens the web page
 driver.get(target_url)
 
-# wait x seconds before searching for the consent button
-time.sleep(3)
+# getting the info from the first row in the table. Index 0 is table head, index 1 is folder, date, etc.
+table = driver.find_element("xpath", '//*[@id="files_list"]/tbody/tr[1]')
 
-# locate and click the "do not accept" button
-consent_button = driver.find_element_by_xpath('//*[@id="cmpwelcomebtnno"]/a')
-consent_button.click()
-
-# getting all the info from the table
-table = driver.find_elements_by_xpath('//*[@id="files_list"]')
-
-full_info = ""
-for items in table:
-    full_info = items.text
-
-# splitting the string into list, because the 6th element (index 5)
+# table.text returns string separated by new lines
+# splitting the string into list, because the 1st element (index 0)
 # #contains the last folder name and last date of modification
-full_info = full_info.split("\n")
-current_name, found_date = full_info[5].split()
+full_info = table.text.split("\n")[0]
 
-# extracting year, month, day from found_date string
-found_year, found_month, found_day = [int(x) for x in found_date.split("-")]
+# creating variables for the founded elements - folder, date (or hours) since last modification
+current_name = ""
+found_date = ""
+new_folder_found = False
 
-# getting the difference in days
-# (it gaves something like "2 days, 14:05:28.657927", which is translated to number with ".days" in the "if" statement)
-days_difference = datetime.now() - datetime(found_year, found_month, found_day)
 
-if days_difference.days < 4:
-    print(f"Modified folder found!\nName: {current_name}\nDate: {found_day}.{found_month}.{found_year}")
-    print(f'Download link: {target_url+current_name}')
+def check_date(folder_date_list, new_folder_checker):
+    folder_name, date = folder_date_list
+
+    # extracting year, month, day from found_date string
+    found_year, found_month, found_day = [int(x) for x in found_date.split("-")]
+
+    # getting the difference in days (it gaves something like "2 days, 14:05:28.657927", which is translated to
+    # number with ".days" in the "if" statement)
+    days_difference = datetime.now() - datetime(found_year, found_month, found_day)
+    if days_difference.days < 4:
+        new_folder_checker = True
+        date = f"{found_year}-{found_month}-{found_day}"
+    return folder_name, date, new_folder_checker
+
+
+# checking if the folder is newly created ( last 24 hours )
+if " < " in full_info:
+    current_name, found_date = full_info.split(" < ")
+    new_folder_found = True
+else:
+    current_name, found_date, new_folder_found = check_date(full_info.split(), new_folder_found)
+
+
+if new_folder_found:
+    print(f"Modified folder found!\nName: {current_name}\nDate: {found_date}")
+    print(f'Download link: {target_url + current_name}')
+
 else:
     print(f"Everything is the same as in {found_date}\nLast created folder is {current_name}\nBetter luck next time!")
