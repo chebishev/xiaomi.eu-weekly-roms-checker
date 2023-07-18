58a5b48f0aec2abede4c199978f3861d506a7a1e
chebishev <atanas.chebishev@gmail.com>
2023-07-18 10:34:40 +0300

    changed scrapper to Beautiful Soup. Now everything is faster and with less code.

--- sourceforge_new_folder_checker.py
+++ sourceforge_new_folder_checker.py
@@ -1,51 +1,75 @@
 from datetime import datetime
-
-from selenium import webdriver
-from selenium.webdriver.edge.options import Options
-from selenium.webdriver.edge.service import Service as EdgeService
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+import requests
+from bs4 import BeautifulSoup
 
 
-def get_driver():
-    # options - in order to make headless search with Edge
-    options = Options()
-    options.use_chromium = True
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument('--allow-running-insecure-content')
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--log-level=3')  # remove errors about "Error with Feature-Policy header
-
-    # get and install the newest, needed driver in .root/.wdm
-    service = EdgeService(EdgeChromiumDriverManager().install())
-
-    return webdriver.Edge(service=service, options=options)
+# from selenium import webdriver
+# from selenium.webdriver.edge.options import Options
+# from selenium.webdriver.edge.service import Service as EdgeService
+# from webdriver_manager.microsoft import EdgeChromiumDriverManager
+#
+#
+# def get_driver():
+#     # options - in order to make headless search with Edge
+#     options = Options()
+#     options.use_chromium = True
+#     options.add_argument("--headless")
+#     options.add_argument("--disable-gpu")
+#     options.add_argument('--allow-running-insecure-content')
+#     options.add_argument('--ignore-certificate-errors')
+#     options.add_argument('--log-level=3')  # remove errors about "Error with Feature-Policy header
+#
+#     # get and install the newest, needed driver in .root/.wdm
+#     service = EdgeService(EdgeChromiumDriverManager().install())
+#
+#     return webdriver.Edge(service=service, options=options)
 
 
 def telegram_message():
-    driver = get_driver()
-
-    # set the url that we will check for new folders
-    target_url = "https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/files/xiaomi.eu/MIUI-WEEKLY-RELEASES/"
-
-    # opens the web page
-    driver.get(target_url)
+    # driver = get_driver()
+    #
+    # # set the url that we will check for new folders target_url =
+    # "https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/files/xiaomi.eu/MIUI-WEEKLY-RELEASES/"
+    #
+    # # opens the web page
+    # driver.get(target_url)
 
     # getting the info from the first row in the table. Index 0 is table head, index 1 is folder, date, etc.
-    table = driver.find_element("xpath", '//*[@id="files_list"]/tbody/tr[1]')
+    # table = driver.find_element("xpath", '//*[@id="files_list"]/tbody/tr[1]')
 
     # table.text returns string separated by new lines
     # splitting the string into list, because the 1st element (index 0)
     # #contains the last folder name and last date of modification
-    full_info = table.text.split("\n")[0]
+    # full_info = table.text.split("\n")[0]
+
+    # url needed for beautiful soup to do its magic:
+    url = "https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/files/xiaomi.eu/MIUI-WEEKLY-RELEASES/"
+    page = requests.get(url)
+
+    # BeautifulSoup instance that gets url and parser as arguments
+    soup = BeautifulSoup(page.content, "html.parser")
+    # getting the info directly from the html by id of the table
+    results = soup.find(id="files_list")
+    # getting all table rows with this class in order to get the folder name and last modification date (or hours)
+    folders = results.find_all('tr', class_="folder")
+    # this variable will contain the result of the first valid row
+    # (that isn't cointain "Parent Folder" or other irrelevant information)
+    full_info = ""
+
+    # getting all the rows, but we need only the first valid one since it is the folder that we are checking
+    for folder in folders:
+        if "Parent" in folder.text:
+            continue
+        full_info = folder.text.split()
+        break
 
     # creating variables for the founded elements - folder, date (or hours) since last modification
     current_name = ""
     found_date = ""
     new_folder_found = False
 
-    def check_date(folder_date_list, new_folder_checker):
-        folder_name, date = folder_date_list
+    def check_date(folder_found, date_found, new_folder_checker):
+        folder_name, date = folder_found, date_found
 
         # extracting year, month, day from found_date string
         found_year, found_month, found_day = [int(x) for x in date.split("-")]
@@ -60,14 +84,14 @@ def telegram_message():
 
     # checking if the folder is newly created ( last 24 hours )
     if " < " in full_info:
-        current_name, found_date = full_info.split(" < ")
+        current_name, found_date = full_info[0], full_info[1]
         new_folder_found = True
     else:
-        current_name, found_date, new_folder_found = check_date(full_info.split(), new_folder_found)
+        current_name, found_date, new_folder_found = check_date(full_info[0], full_info[1], new_folder_found)
 
     if new_folder_found:
         output = f"Modified folder found!\nName: {current_name}\nDate: {found_date}\n" \
-                 f""f"Download link: {target_url + current_name}"
+                 f""f"Download link: {url + current_name}"
     else:
         output = f"Everything is the same as in {found_date}\n" \
                  f"Last created folder is {current_name}\nBetter luck next time!"
