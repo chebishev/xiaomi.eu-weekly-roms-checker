1ac1cce6e454b37000435ed4326633a981fd8f31
chebishev <atanas.chebishev@gmail.com>
2024-08-23 11:31:43 +0300

    add scrapeops random user agents initial scraped folders saved in dictionary in file fix getting correct filenames and links

--- get_afh_folders.py
+++ get_afh_folders.py
@@ -1,6 +1,5 @@
-import requests
-
 from scraper import scrape_config_by_url
+from all_afh_folders import androidfilehost_xiaomieu_folders
 
 
 def get_all_folders(url, rom_type):
@@ -21,26 +20,30 @@ main_url = "https://androidfilehost.com"
 all_folders_url = "https://androidfilehost.com/?w=files&flid=18823"
 stable_url = "https://androidfilehost.com/?w=files&flid=36337"
 
-all_folders = {
-    "weekly": get_all_folders(all_folders_url, "weekly"),
-    "stable": get_all_folders(stable_url, "stable")
-}
-counter = 0
-for folder_type, inner_folders in all_folders.items():
+# all_folders = {
+#     "weekly": get_all_folders(all_folders_url, "weekly"),
+#     "stable": get_all_folders(stable_url, "stable")
+# }
+all_afh_folders = androidfilehost_xiaomieu_folders
+final_dict = {}
+failed_links = []
+for folder_type, inner_folders in all_afh_folders.items():
     for current_folder, url in inner_folders.items():
+        if current_folder not in final_dict:
+            final_dict[current_folder] = []
         new_url = main_url + url
-        folder_soup = scrape_config_by_url(new_url)
         try:
-            folder_results = folder_soup.find(class_="file-name")
-            element = folder_results.find("a")
-            # prints the result in format:
-            # 20.1.15 -> xiaomi.eu_multi_MINote10_MICC9Pro_20.1.15_v11-9.zip - https://androidfilehost.com/?fid=4349826312261703062
-            # print(f"{current_folder} -> {element.text} - {main_url+ element['href']}")
-            counter += 1
+            folder_soup = scrape_config_by_url(new_url)
+            folder_results = folder_soup.find_all(class_="file")
+            for element in folder_results:
+                current_element = element.find("a")
+                # # prints the result in format:
+                # # 20.1.15 -> xiaomi.eu_multi_MINote10_MICC9Pro_20.1.15_v11-9.zip - https://androidfilehost.com/?fid=4349826312261703062
+                final_dict[current_folder].append(current_element.text)
+                final_dict[current_folder].append(current_element['href'])
         except Exception as e:
-            print("### ERROR ###")
-            print(e)
-            print(f"{current_folder} -> {url}")
+            print(f"{current_folder} - {url} - {e}")
+            failed_links.append(new_url)
 
-
-print(counter)
+print(final_dict)
+print(failed_links)
--- scraper.py
+++ scraper.py
@@ -1,9 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+def get_random_user_agent():
+    response = requests.get(
+        url='https://headers.scrapeops.io/v1/user-agents',
+        params={
+            'api_key': os.getenv('SCRAPEOPS_API_KEY'),
+            'num_results': '1'}
+    )
+
+    return response.json()["result"][0]
 
 
 def scrape_config_by_url(url):
-    page = requests.get(url)
+    page = requests.get(url, headers={'User-Agent': get_random_user_agent()})
 
     # BeautifulSoup instance that gets url and parser as arguments
     soup = BeautifulSoup(page.content, "html.parser")
