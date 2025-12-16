acad4ed6cab255f46b99b33538b88bb20fe67198
chebishev <atanas.chebishev@gmail.com>
2025-12-16 15:28:50 +0200

    todo: Fill HyperOS 1 and 2 jsons

---
+++ devices_by_os_version/check_json.py
@@ -0,0 +1,42 @@
+import json
+import sys
+sys.path.append("../")
+
+main_link = "https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/files/xiaomi.eu/"
+links_to_files = {
+    "HyperOS 1.0.json": f"{main_link}HyperOS-STABLE-RELEASES/HyperOS1.0/",
+    "HyperOS 2.0.json": f"{main_link}HyperOS-STABLE-RELEASES/HyperOS2.0/",
+    "HyperOS 3.0.json": f"{main_link}HyperOS-STABLE-RELEASES/HyperOS3.0/",
+    "MIUI 12.json": f"{main_link}MIUI-STABLE-RELEASES/MIUIv12/",
+    "MIUI 13.json": f"{main_link}MIUI-STABLE-RELEASES/MIUIv13/",
+    "MIUI 14.json": f"{main_link}MIUI-STABLE-RELEASES/MIUIv14/",
+
+}
+
+with open("HyperOS 3.0.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+chosen_model = data["Xiaomi 15"]["rom_name"]
+
+from scraper import scrape_config_by_url as scrape
+
+soup = scrape(links_to_files["HyperOS 3.0.json"])
+rows = soup.find_all("tr", class_="file")
+
+for row in rows:
+    a = row.find("a", href=True)
+    if not a:
+        continue
+   
+    name = a["href"]
+    # skip the "Parent folder" row
+    if name == "..":
+        continue
+
+    if chosen_model not in name:
+        continue
+
+    print(name)
+    break
+else:
+    print("no such model found")
